bfde35a21590a94d97da0cc5ea2cacfccca90d15
dawn_lch <chung4111@naver.com>
2024-03-11 21:55:56 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) {
+        String[] record={"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+        Solution sol=new Solution();
+        sol.solution(record);
+    }
+}
+class Solution {
+    public String[] solution(String[] record) {
+        String[] answer = {};
+        return answer;
+    }
+}
